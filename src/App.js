544f7d23430c525571c786b3ba465aae85e53c8f
“Ashish <V553973@amerisourcebergen.com>
2023-09-23 17:45:32 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           I am crime master Go Go!!
         </p>
-        
-      </header>
-    </div>
   );
 }
 
